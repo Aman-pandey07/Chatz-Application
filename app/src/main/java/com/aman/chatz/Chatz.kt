1ab645490a53b1a79d4a2c5e0439ae02860df5bb
Aman-pandey07 <99860796+Aman-pandey07@users.noreply.github.com>
2024-09-05 16:02:04 +0530

    login signup and firebase authentication donecand working properly

---
+++ app/src/main/java/com/aman/chatz/Chatz.kt
@@ -0,0 +1,10 @@
+package com.aman.chatz
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+
+@HiltAndroidApp
+class Chatz:Application() {
+}
--- app/src/main/java/com/aman/chatz/ui/theme/MainAppComposable.kt
+++
@@ -1,9 +0,0 @@
-package com.aman.chatz.ui.theme
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun MainApp() {
-    
-}
